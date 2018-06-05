d333e1f327f7f2c4461d6e68075f2e7fefbc00a4
Leonardo Smoginski <dev@leoginski.user>
2018-06-05 00:37:39 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Interpreter
+[Apresentação reveal.js](http://lucaslagrimante.github.io/Interpreter)  
+
+Exemplo prático do Padrão de Projeto Interpreter feito em Java.  
+(Practical example of the Design Pattern Interpreter made in Java.)
+
+![Diagrama Interpreter](https://upload.wikimedia.org/wikipedia/commons/2/21/Interpreter_design_pattern.png)
+
+REFS:  
+[https://sourcemaking.com/design_patterns/interpreter](https://sourcemaking.com/design_patterns/interpreter)  
+[https://pt.wikipedia.org/wiki/Interpreter](https://pt.wikipedia.org/wiki/Interpreter)  
+[https://www.youtube.com/watch?v=hmX2s3pe_qk](https://www.youtube.com/watch?v=hmX2s3pe_qk)  
+[http://alumni.media.mit.edu/~tpminka/patterns/Interpreter.html](http://alumni.media.mit.edu/~tpminka/patterns/Interpreter.html)
---
+++ src/interpreter/Interpreter.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interpreter;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Interpreter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String roman = JOptionPane.showInputDialog("Digite um número romano!");
+
+        if (roman == null || roman.equals("")) {
+            JOptionPane.showMessageDialog(null, "Você não digitou nada.");
+        } else {
+
+            Context context = new Context(roman);
+
+            // Build the 'parse tree'
+            List<Expression> trees = new ArrayList<>();
+
+            trees.add(new ThousandExpression());
+            trees.add(new HundredExpression());
+            trees.add(new TenExpression());
+            trees.add(new OneExpression());
+
+            trees.forEach((tree) -> {
+                tree.Interpret(context);
+            });
+            
+            System.err.println(context.getOutput());
+            
+            JOptionPane.showMessageDialog(null, context.getOutput());
+        }
+    }
+}
